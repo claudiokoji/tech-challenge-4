8e4758fe147c1be363b397861fe243e58a88a282
Walcow, Nelson <Nelson.Walcow@br.experian.com>
2024-07-13 21:04:43 -0300

    add text

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ st.subheader(
 )
 st.markdown(
     """
-    ??????
+    A análise histórica do petróleo Brent revela uma forte influência de fatores geopolíticos, econômicos e tecnológicos em seus preços. Para prever o futuro, é essencial considerar uma combinação desses fatores, juntamente com mudanças nas políticas de energia e avanços tecnológicos. Embora seja impossível prever com precisão exata, a compreensão desses elementos pode ajudar a fazer previsões mais informadas
     """
 )
 
@@ -34,6 +34,12 @@ st.markdown(
 st.subheader(":blue[Metodologia]", divider="blue")
 st.markdown(
     """
-    ?????????;
+    •	Análise Técnica: Uso de padrões históricos de preços e indicadores técnicos para prever movimentos futuros.
+    •	Modelagem Econômica: Modelos econométricos que consideram variáveis macroeconômicas e setoriais.
+    •  	Simulações e Cenários: Criação de cenários baseados em diferentes suposições sobre variáveis-chave.
+Perspectivas para o Futuro
+    •	Curto Prazo: Pode haver volatilidade devido a incertezas geopolíticas e flutuações na demanda pós-pandemia.
+    •	Médio a Longo Prazo: A transição energética global para fontes renováveis pode resultar em uma demanda estruturalmente menor por petróleo, pressionando os preços para baixo, embora a volatilidade permaneça devido a fatores geopolíticos e econômicos.
+
 """
 )
